608642c78585300d7a24e8e8a1e54144afdfcfa2
dmitriy <vilaps04@mail.ru>
2017-01-03 15:28:02 +0200

    first commit

---
+++ Рабочий стол/HTML/my-sait4/js/popup.js
@@ -0,0 +1,45 @@
+        var link = document.querySelector(".login");
+		var popup = document.querySelector(".modal-content");
+		var close = popup.querySelector(".modal-content-close");
+		var form = popup.querySelector("form");
+		var login = popup.querySelector("[name=login]");
+		var password = popup.querySelector("[name=password]");
+		var storage = localStorage.getItem("login");
+		
+		link.addEventListener("click", function(event) {
+		event.preventDefault();
+		popup.classList.add("modal-content-show");
+		/* убираем фокус логина на пароль login.focus();*/
+		if (storage) {
+		  login.value = storage;
+		  password.focus();
+		} else {
+		login.focus();
+		}
+		});
+		
+		close.addEventListener("click", function(event) {
+		event.preventDefault();
+		popup.classList.remove("modal-content-show");
+		popup.classList.remove("modal-error");
+		});
+		
+		form.addEventListener("submit", function(event) {
+		 if (!login.value || !password.value) {
+		event.preventDefault();
+		/*console.log("Ќужно ввести логин и пароль");*/
+		popup.classList.add("modal-error");
+		}	else {
+		localStorage.setItem("login", login.value);
+		}	/*сохранение браузером логина*/
+		});
+		
+		window.addEventListener("keydown", function(event) {
+		  if (event.keyCode === 27) {
+		    if (popup.classList.contains("modal-content-show")) {
+			  popup.classList.remove("modal-content-show");
+			}
+		  }
+		});
+		
+		
